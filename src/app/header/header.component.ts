6bb0da46a5349a282da92cc953aa7a8db378620f
Dharmendrasinh Chudasama <dpchudasama1@gmail.com>
2020-09-17 18:10:19 +0530

    Dynamic nav-bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MyRoute } from '../app-routing.module';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  @Input('routes') routes: any[];
+  routes: any[] = []; //fill dynamically by constructor
 
   constructor(router:Router) {
-    // router.events.subscribe(evt=>{
-    //   console.log(evt);
-    // });
-  }
-
-  ngOnInit(): void {
-    this.routes.forEach(console.log);
+    router.config
+      .map((route:MyRoute)=>route)
+      .filter(route=>route.label!=null && route.path!=null)
+      .forEach((route)=>{
+        this.routes.push({
+          label: route.label,
+          url: route.path
+        });
+      });
   }
 
 }
